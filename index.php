75d2f9b5da4bf013d3ca5418d799dde49c6444d9
kristao <kristao777@gmail.com>
2025-08-11 18:36:03 +0200

    correctfis_divers

--- index.php
+++ index.php
@@ -21,3 +21,25 @@
     
     // ajout du pied de page
     require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'footer.php');
+
+    // connexion à la base de données
+    require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Models'.DIRECTORY_SEPARATOR.'connectDb.php');
+    
+    // ajout de l'en tête
+    require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'header.php');
+
+    $route = isset($_GET['c'])? $_GET['c'] : 'home';
+    
+    switch ($route) {
+        case 'home':
+            require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Controllers'.DIRECTORY_SEPARATOR.'homeController.php');
+            break;
+        case 'contact':
+            require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Controllers'.DIRECTORY_SEPARATOR.'contactController.php');
+            break;
+        default:
+            echo "Page non trouvée";
+    }
+    
+    // ajout du pied de page
+    require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'footer.php');
