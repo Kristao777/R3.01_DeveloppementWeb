7fac8bf7af07cd329b9dcf656b60ad4e9069c3e8
kristao <kristao777@gmail.com>
2025-08-12 10:40:30 +0200

    correctifs_suite

--- src/Views/Recette/liste.php
+++ src/Views/Recette/liste.php
@@ -3,9 +3,9 @@
 <div class="row">
     <!-- Boucle permettant de lister les recettes -->
     <?php foreach ($recipes as $recipe) : ?>
-        <div class="recipe col-4 p-2" data-id="<?php echo $recipe['id']; ?>">
+        <div class="col-4 p-2">
             <!-- Utilisation des Cards Bootstrap -->
-            <div class="card">
+            <div class="recipe card" data-id="<?php echo $recipe['id']; ?>">
                 <div class="card-body">
                     <img src="<?php echo $recipe['image'] != '' ? 'upload'.DIRECTORY_SEPARATOR.$recipe['image'] : 'upload'.DIRECTORY_SEPARATOR.'no_image.png' ;?>" alt="<?php echo $recipe['titre'];?>" class="card-img-top">
                     <h2 class="card-title"><?php echo $recipe['titre']; ?></h2>
