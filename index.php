66bc19ebe740cc3fc9b1b6510bc66a08eca62075
kristao <kristao777@gmail.com>
2025-08-14 18:05:20 +0200

    correctifs_models

--- index.php
+++ index.php
@@ -11,9 +11,6 @@
     // import de la classe UserController
     require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Controllers'.DIRECTORY_SEPARATOR.'UserController.php');
     
-    // connexion à la base de données
-    require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Models'.DIRECTORY_SEPARATOR.'connectDb.php');
-    
     // ajout de l'en tête
     require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'header.php');
 
@@ -69,16 +66,16 @@
                     $userController->inscription();
                     break;
                 case 'inscrire':
-                    $userController->enregistrer($pdo);
+                    $userController->enregistrer();
                     break;
                 case 'connexion':
                     $userController->connexion();
                     break;
                 case 'connecter':
-                    $userController->verifieConnexion($pdo);
+                    $userController->verifieConnexion();
                     break;
                 case 'profil':
-                    $userController->profil($pdo);
+                    $userController->profil();
                     break;
                 case 'deconnexion':
                     $userController->deconnexion();
